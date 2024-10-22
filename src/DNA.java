060a8c034d7a036ec27a659ca91ff5f3130c6a81
codykletter <cody.kletter@menloschool.org>
2024-10-22 13:11:55 -0700

    final DNA 10/22

--- src/DNA.java
+++ src/DNA.java
@@ -24,26 +24,35 @@ public class DNA {
     public static int STRCount(String sequence, String STR) {
         int totalCons = 0;
         int strLength = STR.length();
+        // Finds the hash for the initial set of characters in the sequence (length of STR)
         int seqHash = hash(sequence.substring(0, strLength), strLength);
+        // Finds the hash for the STR given
         int strHash = hash(STR, strLength);
         // Calculates number to shift the char by for removal of left-most letter
         int bitShift = (strLength - 1) * 2;
         int seqLength = sequence.length();
-        //
+        // Loop through each character in the sequence
         for (int i = 0; i < seqLength - strLength; i++) {
             int tempCount = 0;
-            int currentNum = seqHash;
+            int currentHash = seqHash;
             int currentIndex = i;
+            // Loops for each number of occurrences of STR from starting index
             while (true) {
-                if (currentNum == strHash) {
+                // If the str matches the current hash, increases count by 1
+                if (currentHash == strHash) {
                     tempCount++;
                     currentIndex += strLength;
+                    // Hash the next digits in the sequence if possible
                     try {
-                        currentNum = hash(sequence.substring(currentIndex, currentIndex + strLength), strLength);
-                    } catch (IndexOutOfBoundsException e) {
-                        currentNum = -1;
+                        currentHash = hash(sequence.substring(currentIndex, currentIndex + strLength), strLength);
                     }
-                } else {
+                    // Sets current hash to -1 so no match will be found on next iteration
+                    catch (IndexOutOfBoundsException e) {
+                        currentHash = -1;
+                    }
+                }
+                // If the sequence doesn't match, finds the next hash in constant time and updates total STR counts
+                else {
                     seqHash = nextHash(sequence, i, seqHash, strLength, bitShift);
                     totalCons = Math.max(tempCount, totalCons);
                     break;
@@ -54,7 +63,7 @@ public class DNA {
     }
 
     /**
-     * Given a sequence of DNA, determines and returns the number equivalent of that sequence using our number constants
+     * Given a sequence of DNA, determines and returns the hash of that sequence using Polynomial Rolling Hash Function
      * @param sequence sequence of DNA (a, t, c, g)
      * @param strLength the length of the STR being searched for
      * @return the number representation of the sequence
@@ -63,19 +72,20 @@ public class DNA {
         // Loops through each letter in the sequence
         int hash = 0;
         for (int i = 0; i < strLength; i++) {
-            // Allocates space in the number for the DNA base
+            // Adds to hash using Horner's method
             hash = (hash * RADIX + Character.toUpperCase(sequence.charAt(i)));
         }
         return hash;
     }
     /**
-     * Takes in a number representation of a sequence and a current index, and outputs the "next window," or the
+     * Takes in a hash of a sequence and a current index, and outputs the "next hash," the
      * number representation of the sequence beginning at index one to the right
      * @param sequence entire sequence of DNA
      * @param currentIndex index of the most recent window of DNA looked at
-     * @param windowNum the number representation of the last looked at window of DNA
+     * @param seqHash the number representation of the last looked at window of DNA
      * @param strLength length of given STR being searched for
-     * @return number representation of next window
+     * @param bitShift number to bit shift by to remove left-most value
+     * @return hash of next sequence
      */
     public static int nextHash(String sequence, int currentIndex, int seqHash, int strLength, int bitShift) {
         int firstCharVal = sequence.charAt(currentIndex);
